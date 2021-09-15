a808321aa9a2d104eed20c4caede7d7529efc165
Heikki Rantala <heikki.rantala@aalto.fi>
2021-09-15 13:50:44 +0300

    Show all (max 200) videos on results

--- src/client/reducers/veterans/videos.js
+++ src/client/reducers/veterans/videos.js
@@ -8,7 +8,7 @@ export const INITIAL_STATE = {
   paginatedResultsSparqlQuery: null,
   resultCount: 0,
   page: -1,
-  pagesize: 25,
+  pagesize: 200,
   sortBy: null,
   sortDirection: null,
   fetching: false,
