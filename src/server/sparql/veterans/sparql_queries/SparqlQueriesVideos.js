2b4dec29f0235e9283c2980c48670257725e0d3a
esikkala <esko.ikkala@aalto.fi>
2021-10-06 11:19:12 +0300

    Video instance query: get end time in seconds

--- src/server/sparql/veterans/sparql_queries/SparqlQueriesVideos.js
+++ src/server/sparql/veterans/sparql_queries/SparqlQueriesVideos.js
@@ -45,6 +45,7 @@ export const videoPropertiesInstancePage =
     {
       ?id :structured_content ?timeSlice__id .
       ?timeSlice__id  :begin_timestamp ?beginTimestamp ;
+                      :end_timestamp ?endTimestamp ;
                       :order ?timeSlice__order ;
                       :text_content ?timeSlice__textContent .
 
@@ -53,53 +54,56 @@ export const videoPropertiesInstancePage =
                                 :text_content ?timeSlice__textSlice__textContent . 
 
       BIND(CONCAT(SUBSTR(?timeSlice__textContent, 1, 50), '...') as ?timeSlice__prefLabel)                
-      BIND(HOURS(?beginTimestamp) as ?hours)
-      BIND(MINUTES(?beginTimestamp) as ?minutes)
-      BIND(SECONDS(?beginTimestamp) as ?seconds)
-      BIND(CONCAT(STR(?hours), ':', STR(?minutes), ':', STR(xsd:integer(?seconds))) as ?timeSlice__beginTimeLabel)
-      BIND(?hours * 60 * 60 + ?minutes * 60 + ?seconds as ?beginTimeInSeconds_)
-      BIND(xsd:integer(?beginTimeInSeconds_) as ?timeSlice__beginTimeInSeconds)                  
+      
+      BIND(HOURS(?beginTimestamp) as ?beginHours)
+      BIND(MINUTES(?beginTimestamp) as ?beginMinutes)
+      BIND(SECONDS(?beginTimestamp) as ?beginSeconds)
+      BIND(CONCAT(STR(?beginHours), ':', STR(?beginMinutes), ':', STR(xsd:integer(?beginSeconds))) as ?timeSlice__beginTimeLabel)
+      BIND(?beginHours * 60 * 60 + ?beginMinutes * 60 + ?beginSeconds as ?beginTimeInSeconds_)
+      BIND(xsd:integer(?beginTimeInSeconds_) as ?timeSlice__beginTimeInSeconds) 
+      
+      BIND(HOURS(?endTimestamp) as ?endHours)
+      BIND(MINUTES(?endTimestamp) as ?endMinutes)
+      BIND(SECONDS(?endTimestamp) as ?endSeconds)
+      BIND(CONCAT(STR(?endHours), ':', STR(?endMinutes), ':', STR(xsd:integer(?endSeconds) - 1)) as ?timeSlice__endTimeLabel)
+      BIND(?endHours * 60 * 60 + ?endMinutes * 60 + ?endSeconds as ?endTimeInSeconds_)
+      BIND(xsd:integer(?endTimeInSeconds_) - 1 as ?timeSlice__endTimeInSeconds) 
     }
 `
 
-export const videoPropertiesFacetResults =
-  `?id :interviewed_person/skos:prefLabel ?prefLabel__id .
-   #?id :interviewed_person/foaf:givenName ?givenName .
-   #BIND (CONCAT(?familyName, ', ') AS ?labelBeginning)
-   #BIND (CONCAT(?labelBeginning, ?givenName) AS ?prefLabel__id)
-   #?id skos:prefLabel ?prefLabel__id .
-   BIND (?prefLabel__id as ?prefLabel__prefLabel)
-   BIND(CONCAT("/${perspectiveID}/page/", REPLACE(STR(?id), "^.*\\\\/(.+)", "$1")) AS ?prefLabel__dataProviderUrl)
-      {
-        ?id :place_of_interview ?placeOfInterview .
-      }
-      UNION
-      {
-        ?id :length ?raw_length .
-        BIND(HOURS(?raw_length) AS ?hours)
-        BIND(MINUTES(?raw_length) AS ?minutes)
-        BIND(SECONDS(?raw_length) AS ?seconds)
-        BIND(
-          CONCAT(
-              CONCAT(STR(?hours), 'h '), CONCAT(STR(?minutes), 'm')
-            ) AS ?length
-          )
-      }
-      UNION
-      {
-        ?id :keyword ?keyword__id .
-        ?keyword__id skos:prefLabel ?keyword__prefLabel .
-        ?keyword__id :uri ?keyword__dataProviderUrl .
-      }
-      #UNION
-      #{
-      #  ?id :structured_content/:text_slice/:text_content ?content__id .
-      #  BIND (?content__id AS ?content__prefLabel )
-      #}
-      UNION
-      {
-        ?id :named_entity ?namedEntity__id .
-        ?namedEntity__id skos:prefLabel ?namedEntity__prefLabel .
-      }
-
+export const videoPropertiesFacetResults = `
+  ?id :interviewed_person/skos:prefLabel ?prefLabel__id .
+  BIND (?prefLabel__id as ?prefLabel__prefLabel)
+  BIND(CONCAT("/${perspectiveID}/page/", REPLACE(STR(?id), "^.*\\\\/(.+)", "$1")) AS ?prefLabel__dataProviderUrl)
+  {
+    ?id :place_of_interview ?placeOfInterview .
+  }
+  UNION
+  {
+    ?id :length ?raw_length .
+    BIND(HOURS(?raw_length) AS ?hours)
+    BIND(MINUTES(?raw_length) AS ?minutes)
+    BIND(SECONDS(?raw_length) AS ?seconds)
+    BIND(
+      CONCAT(
+        CONCAT(STR(?hours), 'h '), CONCAT(STR(?minutes), 'm')
+      ) AS ?length
+    )
+  }
+  UNION
+  {
+    ?id :keyword ?keyword__id .
+    ?keyword__id skos:prefLabel ?keyword__prefLabel .
+    ?keyword__id :uri ?keyword__dataProviderUrl .
+  }
+  #UNION
+  #{
+  #  ?id :structured_content/:text_slice/:text_content ?content__id .
+  #  BIND (?content__id AS ?content__prefLabel )
+  #}
+  UNION
+  {
+    ?id :named_entity ?namedEntity__id .
+    ?namedEntity__id skos:prefLabel ?namedEntity__prefLabel .
+  }
 `
